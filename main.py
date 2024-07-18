d4c16bd0ae9f2e3c827c1ccea8af5437f5eb6542
Navy <80156564+asdalige@users.noreply.github.com>
2024-07-18 10:29:25 +0800

    .

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask
-
+import os
+ 
 app = Flask(__name__)
-
+ 
 @app.route('/')
-def home():
-    return 'Hello, World!'
-
+def index():
+    return "Welcome, this is a Flask app deployed on Zeabur"
+ 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080,debug=True)
+    app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
